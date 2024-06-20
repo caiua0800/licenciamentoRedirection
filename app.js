47cc55c2697123a9f99045de73eb4ae78621f320
Dev Mello <caiuabrandao@gmail.com>
2024-06-20 11:34:10 -0300

    chat

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ let dataGlobal = [];
 
 const server = http.createServer((req, res) => {
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    
     if (req.method === 'POST') {
         let body = '';
         req.on('data', chunk => {
